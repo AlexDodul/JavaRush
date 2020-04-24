58b0d36bdd2282159f662b9611513d8084328d31
Alex <alexdodul87@gmail.com>
2020-04-24 20:12:40 +0300

    Shake and all prod

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -1,14 +1,22 @@
 package com.javarush.task.task22.task2210;
 
-/* 
+import java.util.*;
+
+/*
 StringTokenizer
 */
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.asList(getTokens("level22.lesson13.task01", ".")));
     }
 
     public static String[] getTokens(String query, String delimiter) {
-        return null;
+        ArrayList<String> tokens = new ArrayList<>();
+        StringTokenizer str = new StringTokenizer(query,delimiter);
+        while (str.hasMoreTokens()){
+            tokens.add(str.nextToken());
+        }
+        return tokens.toArray(new String[0]);
     }
+
 }
