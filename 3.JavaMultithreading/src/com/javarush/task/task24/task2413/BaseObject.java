7f88f2325a6a4357af97598c4b304bd551b55b59
Alex <alexdodul87@gmail.com>
2020-04-29 20:47:43 +0300

    game Arkanoid is finished

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,41 +1,73 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-    private double x;
-    private double y;
-    private double radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
 
-    public BaseObject(double x, double y, double radius){
+    protected BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
     }
 
-    public double getX(){
+    public double getX() {
         return x;
     }
-    public double getY(){
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
         return y;
     }
-    public double getRadius(){
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
         return radius;
     }
 
-    public void setX(double x){
-        this.x = x;
-    }
-    public void setY(double y){
-        this.y = y;
-    }
-    public void setRadius(double radius){
+    public void setRadius(double radius) {
         this.radius = radius;
     }
 
-    public abstract void draw(Canvas canvas);
-    public abstract void move();
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    abstract void draw(Canvas canvas);
 
-    public boolean isIntersec(BaseObject o){
-        double distance = Math.sqrt(Math.pow((this.x - o.x), 2) + Math.pow((this.y - o.y), 2));
-        return distance <= o.getRadius() || distance <= this.radius;
+    /**
+     * Двигаем себя на один ход.
+     */
+    abstract void move();
+
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    boolean isIntersec(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
     }
 }
